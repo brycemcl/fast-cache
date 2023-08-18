35e2fde896dbe3cf939a63121a051ee96b179aff
brycemclachlan <brycemclachlan@gmail.com>
2023-08-18 15:42:54 -0700

    lz4mt to lz4

--- restore-lz4.sh
+++ restore-lz4.sh
@@ -4,9 +4,9 @@ rm -rf tmp || true
 mkdir tmp || true
 
 start=$(date +%s)
-rclone cat --transfers=120 --checkers=120 --fast-list --multi-thread-streams=20 --multi-thread-cutoff=5M --ignore-checksum cache:fast-cache/$(date +%Y-%m-%d).tar.lz4mt |
+rclone cat --transfers=120 --checkers=120 --fast-list --multi-thread-streams=20 --multi-thread-cutoff=5M --ignore-checksum cache:fast-cache/$(date +%Y-%m-%d).tar.lz4 |
   mbuffer -s 1M -m 512M |
-  lz4mt -dc - |
+  lz4 -dc - |
   mbuffer -s 1M -m 512M |
   tar -xf - -C tmp/
 end=$(date +%s)
