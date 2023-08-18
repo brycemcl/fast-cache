baf41c53abe2d819ddf79d34007237cd75e1e50d
brycemclachlan <brycemclachlan@gmail.com>
2023-08-18 15:01:22 -0700

    remove mbuffer

--- restore.sh
+++ restore.sh
@@ -5,9 +5,7 @@ mkdir tmp || true
 
 start=$(date +%s)
 rclone cat cache:fast-cache/$(date +%Y-%m-%d).tar.zstd |
-  mbuffer -s 1M -m 512M |
   zstd -d - |
-  mbuffer -s 1M -m 512M |
   tar -xf - -C tmp/
 end=$(date +%s)
 runtime=$((end - start))
