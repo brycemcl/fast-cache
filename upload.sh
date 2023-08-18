ae7838c37300d8314f059725cd69208d1061b8d4
brycemclachlan <brycemclachlan@gmail.com>
2023-08-18 14:57:06 -0700

    rclone pt 6

--- upload.sh
+++ upload.sh
@@ -2,9 +2,9 @@
 
 start=$(date +%s)
 tar -c ./node_modules/uploads/ |
-  mbuffer -s 1K -m 512M |
+  mbuffer -s 1M -m 512M |
   zstd --fast=5 - |
-  mbuffer -s 1K -m 512M |
+  mbuffer -s 1M -m 512M |
   rclone rcat cache:fast-cache/$(date +%Y-%m-%d).tar.zstd
 end=$(date +%s)
 runtime=$((end - start))
