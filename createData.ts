1aff75a9e5945cf34091055fd1d7875f929c97bd
brycemclachlan <brycemclachlan@gmail.com>
2023-08-18 19:12:37 -0700

    Use an async pool for data creation

--- createData.ts
+++ createData.ts
@@ -1,22 +1,23 @@
 import { writeFile, mkdir } from 'fs/promises'
 import path from 'path'
-const prom:Promise<any>[] = []
+import asyncPool from 'tiny-async-pool'
+const folders = 100
+const amount = folders * 50000
+const timeStart = Date.now()
 ;(async () => {
-  const timeStart = Date.now()
   await mkdir('node_modules/uploads')
     .catch((e) => {})
     .then(async (err) => {
-      const folders = 100
-      const amount = folders * 50000
-      for (let i = 0; i <= amount; i++) {
-        const folder = String(i % (folders))
+      for await (const amountDone of asyncPool(100, [...Array(amount).keys()], async (i) => {
+        const folder = String(i % folders)
         const localPath = path.join('node_modules/uploads', folder)
         await mkdir(localPath).catch((e) => {})
-        prom.push(writeFile(localPath + '/' + String(i), String(Math.random())))
-        if (i % 10000 === 0) {
-          console.log(`${(Date.now() - timeStart) / 1000} seconds, ${(i / amount) * 100}% started & ${i} files`)
+        await writeFile(localPath + '/' + String(i), String(Math.random()))
+        return i
+      })) {
+        if (amountDone % 10000 === 0) {
+        console.log(`${(Date.now() - timeStart) / 1000} seconds, ${(amountDone / amount) * 100}% started & ${amountDone} files`)
         }
       }
     })
-  await Promise.all(prom)
 })()
